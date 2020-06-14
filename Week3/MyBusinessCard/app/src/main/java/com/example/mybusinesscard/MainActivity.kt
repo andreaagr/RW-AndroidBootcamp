60c4831d9bf78106e3f7155fa50fbe1ef6257b84
Andrea García <andiee.grr@gmail.com>
2020-06-14 05:02:33 -0500

    Circle Image View

--- Week3/MyBusinessCard/app/src/main/java/com/example/mybusinesscard/MainActivity.kt
+++ Week3/MyBusinessCard/app/src/main/java/com/example/mybusinesscard/MainActivity.kt
@@ -2,20 +2,37 @@ package com.example.mybusinesscard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.material.appbar.AppBarLayout
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //---------------------Hide the title of the app
+        //------------------------------------------------------------------------------Hide the title of the app
         toolbar.title = ""
         toolbar.subtitle = ""
-        //----------------------------------------------
-        // Set the toolbar of our application
+        //-------------------------------------------------------------------------------------------------------
+        //---------------------------------------------------------------------Set the toolbar of our application
         setSupportActionBar(toolbar)
+        //-------------------------------------------Hides the profile image when the collapsing bar is collapsed
+        appbar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            if (Math.abs(verticalOffset) - appBarLayout.totalScrollRange == 0) {
+                //  Collapsed
+                img_profile.visibility= View.INVISIBLE
+
+
+            } else {
+                //Expanded
+                img_profile.visibility= View.VISIBLE
+            }
+        })
+        //-------------------------------------------------------------------------------------------------------
 
     }
 }
